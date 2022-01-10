05c3291ff667bb6cb317ac1ddc806ffb4bbdc9a8
D0843969 <wen3131770@gmail.com>
2022-01-10 14:07:59 +0800

    checkstyle

--- src/main/java/fcu/sep/fcushop/service/ProductService.java
+++ src/main/java/fcu/sep/fcushop/service/ProductService.java
@@ -2,12 +2,14 @@ package fcu.sep.fcushop.service;
 
 import fcu.sep.fcushop.database.Sql2oDbHandler;
 import fcu.sep.fcushop.model.Product;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.sql2o.Connection;
 
-import java.util.List;
-
+/**
+ *productservice.
+ */
 @Service
 public class ProductService {
 
@@ -18,28 +20,39 @@ public class ProductService {
 
   }
 
+  /**
+   *productlist.
+   */
   public List<Product> getProducts() {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
-          + " from PRODUCT";
-
+      String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price,"
+          + " DESCRIPTION description from PRODUCT";
       return connection.createQuery(query).executeAndFetch(Product.class);
     }
   }
+
+  /**
+   *keyword product list.
+   */
   public List<Product> getProducts(String keyword) {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
+      String query = "select ID id, NAME name, IMAGE_URL imageUrl, "
+          + "PRICE price, DESCRIPTION description"
           + " from PRODUCT where name like :keyword";
 
       return connection.createQuery(query)
-          .addParameter("keyword", "%"+keyword+"%")
+          .addParameter("keyword", "%" + keyword + "%")
           .executeAndFetch(Product.class);
     }
   }
 
+  /**
+   *insert product.
+   */
   public String insert(String name, String imageUrl, Integer price, String description) {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query = "insert into PRODUCT (NAME,IMAGE_URL,PRICE,DESCRIPTION) values (:name,:imageUrl,:price,:description)";
+      String query = "insert into PRODUCT (NAME,IMAGE_URL,PRICE,DESCRIPTION) "
+          + "values (:name,:imageUrl,:price,:description)";
       System.out.println(query);
       connection.createQuery(query)
           .addParameter("name", name)
@@ -51,6 +64,9 @@ public class ProductService {
     }
   }
 
+  /**
+   *update product.
+   */
   public String update(String name, Integer price) {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
       String query = "update product set PRICE=:price where NAME=:name";
